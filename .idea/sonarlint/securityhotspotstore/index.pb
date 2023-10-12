35c14e9b9e5351087bc7e8e37e69823b18567992
dhirajNS <dhiraj.shetty@neosoftmail.com>
2023-10-12 17:11:02 +0530

    -implmented resilience4j with circuitbreaker and retry instead of hystrix.

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,23 @@
+
+W
+'Microservices/vaccinationCenter/pom.xml,5/3/536bf6c80d44355dfaf85ef56fe1562c0403affd
+≤
+ÅMicroservices/vaccinationCenter/src/main/java/com/vaccinationCenter/vaccinationCenter/controller/VaccinationCenterController.java,3/0/30a500b0b5702cb25b39a3c41f69bbb403c079a9
+ß
+wMicroservices/vaccinationCenter/src/main/java/com/vaccinationCenter/vaccinationCenter/VaccinationCenterApplication.java,4/2/4248e17e48465eaddc297a075c4914891c121223
+†
+pMicroservices/vaccinationCenter/src/main/java/com/vaccinationCenter/vaccinationCenter/repository/CenterRepo.java,b/c/bc85dc573c3fe590f21eb26202c42da1e33515d4
+ã
+[Microservices/apiGateway/src/main/java/com/apiGateway/apiGateway/ApiGatewayApplication.java,4/2/423f18985957d294b56c1150749c51ae03d03c41
+P
+ Microservices/apiGateway/pom.xml,c/c/ccf99479ebf76172d8c0b51bc7ca0859541f7625
+T
+$Microservices/CitizenService/pom.xml,1/6/16b44853977e35080304a8b40063d952d98d6652
+c
+3Microservices/EurekaServer (1)/EurekaServer/pom.xml,c/6/c6e6c4d8811988696ac61c0e75d3cf662c0d55db
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+õ
+kMicroservices/CitizenService/src/main/java/com/citizenService/CitizenService/CitizenServiceApplication.java,c/f/cf426fd7905679ad72f6ea74deaacd9b056b5e8d
+§
+tMicroservices/EurekaServer (1)/EurekaServer/src/main/java/com/eurekaServer/EurekaServer/EurekaServerApplication.java,2/6/269e5385c15f545c4304ca2a4499d8b4397b2f58
--- Microservices/vaccinationCenter/src/main/java/com/vaccinationCenter/vaccinationCenter/controller/VaccinationCenterController.java
+++ Microservices/vaccinationCenter/src/main/java/com/vaccinationCenter/vaccinationCenter/controller/VaccinationCenterController.java
@@ -1,16 +1,19 @@
 package com.vaccinationCenter.vaccinationCenter.controller;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.vaccinationCenter.vaccinationCenter.entity.VaccinationDB;
 import com.vaccinationCenter.vaccinationCenter.model.Citizen;
 import com.vaccinationCenter.vaccinationCenter.model.RequiredResponse;
 import com.vaccinationCenter.vaccinationCenter.repository.CenterRepo;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -22,6 +25,8 @@ public class VaccinationCenterController {
 
     @Autowired
     private RestTemplate restTemplate;
+    public static final String VACCINE_SERVICE="vaccineService";
+    private int attempt=1;
 
     @PostMapping(path ="/add")
     public ResponseEntity<VaccinationDB> addVaccineCenter(@RequestBody VaccinationDB vaccinationCenter) {
@@ -31,7 +36,8 @@ public class VaccinationCenterController {
     }
 
     @GetMapping(path = "/id/{id}")
-    @HystrixCommand(fallbackMethod = "handleFallBack")
+    //@CircuitBreaker(name=VACCINE_SERVICE,fallbackMethod = "handleFallBack")
+    @Retry(name = VACCINE_SERVICE,fallbackMethod = "handleFallBack")
     public ResponseEntity<RequiredResponse> getAllDadaBasedonCenterId(@PathVariable Integer id){
         RequiredResponse requiredResponse =  new RequiredResponse();
         //1st get vaccination center detail
@@ -39,20 +45,28 @@ public class VaccinationCenterController {
         requiredResponse.setCenter(center);
 
         // then get all citizen registerd to vaccination center
-
+        System.out.println("retry method called "+attempt++ +" times "+" at "+new Date());
         java.util.List<Citizen> listOfCitizens = restTemplate.getForObject("http://CITIZEN-SERVICE/citizen/id/"+id, List.class);
+
         requiredResponse.setCitizens(listOfCitizens);
         return new ResponseEntity<RequiredResponse>(requiredResponse, HttpStatus.OK);
     }
 
-    public ResponseEntity<RequiredResponse> handleFallBack(@PathVariable Integer id){
+//    public ResponseEntity<RequiredResponse> handleFallBack(@PathVariable Integer id){
+//        RequiredResponse requiredResponse =  new RequiredResponse();
+//        //1st get vaccination center detail
+//        VaccinationDB center  = centerRepo.findById(id).get();
+//        requiredResponse.setCenter(center);
+//        return new ResponseEntity<RequiredResponse>(requiredResponse, HttpStatus.OK);
+//    }
+
+    public ResponseEntity<RequiredResponse> handleFallBack(Integer id,Exception ex){
         RequiredResponse requiredResponse =  new RequiredResponse();
         //1st get vaccination center detail
         VaccinationDB center  = centerRepo.findById(id).get();
         requiredResponse.setCenter(center);
         return new ResponseEntity<RequiredResponse>(requiredResponse, HttpStatus.OK);
     }
-
 
 
 
